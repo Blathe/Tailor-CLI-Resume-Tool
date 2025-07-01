dbef495e9976a9a4f70e4e964bae37d7fcbe430a
Scott Peters <scottpeters2281@gmail.com>
2025-07-01 12:00:33 -0700

    Did some restructuring and worked on the prompt

--- main.py
+++ main.py
@@ -1,52 +1,60 @@
 from openai import OpenAI
 from markdown_pdf import MarkdownPdf, Section
+import pypandoc
+
+from prompt import tailor_prompt
 
 import json
 
-try:
-    #Load the config file.
-    with open('./config.json', 'r') as f:
-        data = json.load(f)
-
-    #Grab the user's info from the config file.
-    user_info = data['user_info']
-
-    print("Tailor - easily tailor your resume to fit any job posting.")
-    print("Enter a link to the target job posting below.")
-    link = input("Link:")
-
-
-    print("Generating...")
-
-    client = OpenAI(
-      api_key=data['api_key']
-    )
-
-    response = client.responses.create(
-      model="gpt-4.1",
-      tools=[{"type": "web_search_preview"}],
-      input=f"Do not do anything except for what is explained below."
-            f"You are an expert resume writer that has written resumes for top talent."
-            f"You use correct grammar, and all of your resumes generated pass an ATS screener."
-            f"Generate a rich text formatted resume for the following job posting: {link} using my information: {user_info}."
-            f"Do not add anything about references."
-            f"Do not add a summary at the beginning or end of the resume."
-            f"Do not add any skills that the user has not listed. Only use the top 5 relevant skills from the skills pool."
-            f"Do not lie, but you can adjust things to sound better if needed."
-            f"Use Arial as the font."
-            f"Do not add any unnecessary sections that do not exist in the user info section.",
-    )
-
+def get_file_name():
+    '''Prompt the user to enter a file name. Will add more validation later.'''
     file_name = input("Enter a name for your resume:")
+    if len(file_name) == 0:
+        print("File name cannot be empty.")
+        get_file_name()
 
-    pdf = MarkdownPdf()
-    pdf.add_section(Section(response.output_text))
-    pdf.save(f"{file_name}.pdf")
+    return file_name
 
-    print("Your resume has been generated!")
+def run_tailor():
+    try:
+        # Load the config file.
+        with open('./config.json', 'r') as f:
+            data = json.load(f)
 
-except FileNotFoundError:
-    print("Error: 'config.json' not found. Please ensure the file exists in the same directory as your main.py file.")
+        # Grab the user's info from the config file.
+        user_info = data['user_info']
 
-except json.JSONDecodeError:
-    print("Error: Could not decode JSON from 'config.json'. Check file content for valid JSON format.")
+        print("Tailor - easily tailor your resume to fit any job posting.")
+        print("Enter a link to the target job posting below.")
+        job_posting_link = input("Link:")
+
+        print("Generating...")
+
+        client = OpenAI(
+            api_key=data['api_key']
+        )
+
+        response = client.responses.create(
+            model="gpt-4.1",
+            tools=[{"type": "web_search_preview"}],
+            input=f"{tailor_prompt}. Generate a markdown formatted resume for the following job posting: {job_posting_link} using my information: {user_info}."
+        )
+
+        file_name = get_file_name()
+
+        pdf = MarkdownPdf()
+        pdf.add_section(Section(response.output_text))
+        pdf.save(f"{file_name}.pdf")
+
+        print("Your resume has been generated!")
+
+    except FileNotFoundError:
+        print(
+            "Error: 'config.json' not found. Please ensure the file exists in the same directory as your main.py file.")
+
+    except json.JSONDecodeError:
+        print("Error: Could not decode JSON from 'config.json'. Check file content for valid JSON format.")
+
+
+if __name__ == "__main__":
+    run_tailor()
---
+++ prompt.py
@@ -0,0 +1,10 @@
+#define the general instructions for how the application should tailor a resume.
+tailor_prompt = (
+            f"You are an expert resume writer that has written resumes for top talent."
+            f"Please keep the tone professional."
+            f"You are tailoring every part of a resume, to ensure it is compatible with ATS systems, and presents the user in the best light possible."
+            f"You use correct grammar."
+            f"Ensure the user's first and last name are at the top of the resume."
+            f"Do not add anything about references."
+            f"Do not add a summary at the beginning or end of the resume. This should be ready to upload immediately."
+            f"Remove any unnecessary information that does not add value to the provided job posting.")
