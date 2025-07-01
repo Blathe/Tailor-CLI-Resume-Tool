554c1f4173a50e49c10d0fbaaad8422a3a2b1dd2
Scott Peters <scottpeters2281@gmail.com>
2025-06-30 21:45:18 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Tailor - A simple CLI resume tailor.
+
+Tailor was born mainly to prevent my from going insane by tailoring my resume for every single job posting I was applying for.
+
+With this tool, you can easily list all of your skills, education, past jobs, etc. and it will generate a tailored resume using whatever information you provide it that closest matches the job posting.
+
+## How to use
+
+1. Clone the repo
+2. Install requirements - `pip install -r requirements.txt`
+3. Rename the config_example.json file to config.json
+4. Fill out the config file information with your own info and OpenAI API key.
+5. Run Tailor, and paste a job posting link when it asks for one.
+6. ...
+7. Profit with a new tailored resume.
+
+## Notes
+
+Is it perfect? No, not really. But I feel it can give you a good starting point or direction to go when applying to different positions.
+
+## Future features
+
+1. Reading info from an existing resume instead of having to manually fill out the config.
+2. Some better (anything is better than none) error checking.
+3. Allowing users to name the resume before it's generated. (currently it's just a config option)
+
---
+++ main.py
@@ -0,0 +1,49 @@
+from openai import OpenAI
+from markdown_pdf import MarkdownPdf, Section
+
+import json
+
+try:
+    #Load the config file.
+    with open('./config.json', 'r') as f:
+        data = json.load(f)
+
+    #Grab the user's info from the config file.
+    user_info = data['user_info']
+
+    print("Tailor - easily tailor your resume to fit any job posting.")
+    print("Enter a link to the job posting below, and I will tailor your resume to fit the posting.")
+    link = input("Link:")
+
+    client = OpenAI(
+      api_key=data['api_key']
+    )
+
+    response = client.responses.create(
+      model="gpt-4.1",
+      tools=[{"type": "web_search_preview"}],
+      input=f"Do not do anything except for what is explained below."
+            f"You are an expert resume writer that has written resumes for top talent."
+            f"You use correct grammar, and all of your resumes generated pass an ATS screener."
+            f"Generate a rich text formatted resume for the following job posting: {link} using my information: {user_info}."
+            f"Do not add anything about references."
+            f"Do not add a summary at the beginning or end of the resume."
+            f"Do not add any skills that the user has not listed. Only use the top 5 relevant skills from the skills pool."
+            f"Do not lie, but you can adjust things to sound better if needed."
+            f"Use Arial as the font."
+            f"Do not add any unnecessary sections that do not exist in the user info section.",
+    )
+
+    print("Generating...")
+
+    pdf = MarkdownPdf()
+    pdf.add_section(Section(response.output_text))
+    pdf.save(f"{data["resume_file_name"]}.pdf")
+
+    print("Your resume has been generated!")
+
+except FileNotFoundError:
+    print("Error: 'config.json' not found. Please ensure the file exists in the same directory as your main.py file.")
+
+except json.JSONDecodeError:
+    print("Error: Could not decode JSON from 'config.json'. Check file content for valid JSON format.")
